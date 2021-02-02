79fb9ac6f390f54ef0735232eee2790c313cef45
Nijialun <nijialun888@hotmail.com>
2021-02-02 18:04:14 +0800

    initialize project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Fuck />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Fuck from './components/Fuck.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Fuck
+  }
+}
+</script>
---
+++ src/components/Fuck.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    fuck yeah！ 
+  </div>
+</template>
