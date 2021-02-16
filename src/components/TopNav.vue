9caaa4d391b93c6f1b7716ecf342ea30eaa76b0c
Nijialun <nijialun888@hotmail.com>
2021-02-17 00:18:40 +0800

    add TopNav

---
+++ src/components/TopNav.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    导航栏
+    <router-link to="/">Home</router-link>
+    |
+    <router-link to="/doc">文档</router-link>
+  </div>
+  <hr/>
+</template>
+
+<script>
+export default {
+  name: 'TopNav',
+  components: {}
+}
+</script>
+<style lang="scss" scoped>
+  
+</style>
