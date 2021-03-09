d1a5bdb7051145c0de7909ff961bfd41f169bb29
Nijialun <nijialun888@hotmail.com>
2021-03-09 16:13:15 +0800

    添加button，属性绑定

---
+++ src/libs/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <button v-bind="$attrs">
+      <slot />
+    </button>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: "Button",
+  inheritAttrs: false,
+};
+</script>
--- src/views/docs/ButtonDemo.vue
+++ src/views/docs/ButtonDemo.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    <div>Button组件</div>
+    <Button
+      @click="handleClick"
+      size="small"
+    >啊啊</Button>
   </div>
 </template>
+<script lang="ts">
+import Button from "../../libs/Button.vue";
+export default {
+  name: "ButtonDemo",
+  components: {
+    Button,
+  },
+  setup() {
+    const handleClick = () => {
+      console.log("hi");
+    };
+    return { handleClick };
+  },
+};
+</script>
